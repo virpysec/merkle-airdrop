831a12e6e84e3a45eb735d368a71f30a4160ed8f
virpysec <virpysec@proton.me>
2024-08-02 20:42:56 +0800

    Airdop contract using Merkle proofs

---
+++ README.md
@@ -0,0 +1,10 @@
+# Simple Airdrop contract using Merkle Proofs
+
+- Following the Cyfrin Updraft Merkle Airdrop course
+- Using foundry
+
+## Libraries Used
+
+- forge-std
+- foundry-devops
+- murky
---
+++ test/MerkleAirdropTest.t.sol
@@ -0,0 +1,54 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MerkleAirdrop} from "src/MerkleAirdrop.sol";
+import {BagelToken} from "src/BagelToken.sol";
+import {ZkSyncChainChecker} from "lib/foundry-devops/src/ZkSyncChainChecker.sol";
+import {DeployMerkleAirdrop} from "script/DeployMerkleAirdrop.s.sol";
+
+contract MerkleAirdropTest is ZkSyncChainChecker, Test {
+    MerkleAirdrop public airdrop;
+    BagelToken public token;
+
+    bytes32 public ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4; //this root is retrieved from the output.json file
+    uint256 public AMOUNT_TO_CLAIM = 25 * 1e18; //25 tokens (18 decimal places)
+    uint256 public AMOUNT_TO_SEND = AMOUNT_TO_CLAIM * 4;
+    bytes32 proofOne = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
+    bytes32 proofTwo = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] public PROOF = [proofOne, proofTwo];
+    address user;
+    uint256 userPrivKey;
+    address public gasPayer;
+
+    function setUp() public {
+        if (!isZkSyncChain()) {
+            DeployMerkleAirdrop deployer = new DeployMerkleAirdrop();
+            (airdrop, token) = deployer.run();
+        } else {
+            token = new BagelToken();
+            airdrop = new MerkleAirdrop(ROOT, token);
+            token.mint(token.owner(), AMOUNT_TO_SEND); //mint 100 tokens to the owner
+            token.transfer(address(airdrop), AMOUNT_TO_SEND); //transfer 100 tokens to the airdrop contract
+        }
+        (user, userPrivKey) = makeAddrAndKey("user"); //the address of user needs to be present in the merkle tree so testing can be done, this has been put in the generateinput script file
+        gasPayer = makeAddr("gasPayer");
+    }
+
+    function testUsersCanClaim() public {
+        uint256 initialBalance = token.balanceOf(user);
+        bytes32 digest = airdrop.getMessageHash(user, AMOUNT_TO_CLAIM);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPrivKey, digest);
+
+        vm.prank(gasPayer);
+        airdrop.claim(user, AMOUNT_TO_CLAIM, PROOF, v, r, s);
+
+        uint256 endingBalance = token.balanceOf(user);
+
+        console.log("Initial balance: ", initialBalance);
+        console.log("Ending balance: ", endingBalance);
+        assertEq(endingBalance - initialBalance, AMOUNT_TO_CLAIM);
+    }
+}
